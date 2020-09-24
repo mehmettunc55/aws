0793c0d5267487aa61a6e3c407b26a36addf1911
TylerCounter <tyler@clarusway.com>
2020-09-24 05:56:01 -0400

    updated to-do.py

--- projects/003-to-do-web-api-ec2-rds/to-do-api.py
+++ projects/003-to-do-web-api-ec2-rds/to-do-api.py
@@ -11,7 +11,7 @@ db_endpoint = open("/home/ec2-user/dbserver.endpoint", 'r', encoding='UTF-8')
 # Configure mysql database
 app.config['MYSQL_DATABASE_HOST'] = db_endpoint.readline().strip()
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Callahan_1'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'TylerCounter'
 app.config['MYSQL_DATABASE_DB'] = 'todo_db'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 db_endpoint.close()
