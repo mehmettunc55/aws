6c3782d41f82c4e91bc18f5b4373c65f3d75809c
TylerCounter <tyler@clarusway.com>
2020-09-24 06:29:31 -0400

    updated files

--- projects/004-phonebook-web-application/init-phonebook-db.py
+++ projects/004-phonebook-web-application/init-phonebook-db.py
@@ -3,7 +3,7 @@ from mysql.connector import errorcode
 
 config = {
   'user': 'admin',
-  'password': 'Callahan_1',
+  'password': 'TylerCounter',
   'host': 'phonebook-app-db.cbanmzptkrzf.us-east-1.rds.amazonaws.com',
   'database': 'phonebook',
   'raise_on_warnings': True
